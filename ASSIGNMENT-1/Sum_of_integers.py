5f01b7f5d7e2404974c37fac52010a3e85c73ff3
Nitya Shah <ncs110905@gmail.com>
2025-07-30 18:48:55 +0530

    Sum of three integers

---
+++ ASSIGNMENT-1/Sum_of_integers.py
@@ -0,0 +1,10 @@
+def sum_three_integers(a, b, c):
+  if a == b or b == c or a == c:
+    return 0
+  else:
+    return a + b + c
+
+print(f"Sum of (1, 2, 3): {sum_three_integers(1, 2, 3)}")
+print(f"Sum of (2, 2, 3): {sum_three_integers(2, 2, 3)}")
+print(f"Sum of (5, 5, 5): {sum_three_integers(5, 5, 5)}")
+print(f"Sum of (10, -5, 15): {sum_three_integers(10, -5, 15)}")
