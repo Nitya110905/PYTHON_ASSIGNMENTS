eb5e4ed39b397b2e8775adac75179b181437533a
Nitya Shah <ncs110905@gmail.com>
2025-08-04 22:12:49 +0530

    Adding ing or ly to the string

--- ASSIGNMENT-1/Add_ing.py
+++ ASSIGNMENT-1/Add_ing.py
@@ -1,3 +1,8 @@
+# Write a Python program to add 'ing' at the end of a given 
+# string (length should be at least 3). If the given string already 
+# ends with 'ing' then add 'ly' instead if the string length of the 
+# given string is less than 3, leave it unchanged. 
+
 def add_ing(word):
     length = int(len(word))
 
