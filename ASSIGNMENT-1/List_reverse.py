8c699ebba9e10448ed02157723778527038c089d
Nitya Shah <ncs110905@gmail.com>
2025-08-05 21:52:39 +0530

    Reversing a List

---
+++ ASSIGNMENT-1/List_reverse.py
@@ -0,0 +1,10 @@
+# What is List? How will you reverse a list? 
+# -a list is a fundamental data structure that stores a collection of items in a specific order.
+
+
+# Reversing a list :
+my_list = [10, 20, 30, 40, 50]
+print(f"Original list: {my_list}")
+my_list.reverse()
+
+print(f"Reversed list: {my_list}")
