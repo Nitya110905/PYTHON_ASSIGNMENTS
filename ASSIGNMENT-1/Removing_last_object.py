760f76d983c5ce777ab40a3eb1b7667285a4deea
Nitya Shah <ncs110905@gmail.com>
2025-08-05 21:57:00 +0530

    Remove last object from list

---
+++ ASSIGNMENT-1/Removing_last_object.py
@@ -0,0 +1,9 @@
+# How will you remove last object from a list?
+
+fruits = ['apple', 'banana', 'cherry', 'date']
+print(f"Original list: {fruits}")
+
+removed_fruit = fruits.pop()
+
+print(f"Removed item: {removed_fruit}")
+print(f"List after removing the last item: {fruits}")
