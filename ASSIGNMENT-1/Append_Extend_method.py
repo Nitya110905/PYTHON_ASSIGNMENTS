62b03b9677dd409c7cdd935454c9f0c27943b294
Nitya Shah <ncs110905@gmail.com>
2025-08-06 18:46:21 +0530

    List Append and Extend method

--- ASSIGNMENT-1/Append_Extend_method.py
+++ ASSIGNMENT-1/Append_Extend_method.py
@@ -1,5 +1,5 @@
 # Differentiate between append () and extend () methods? 
-
+# -> append adds a singlr object at the end of the list while extend is used to end a block or multiple objects at the end of list.
 
 l = [1 , 2 , 22 , 56 , 90]
 
