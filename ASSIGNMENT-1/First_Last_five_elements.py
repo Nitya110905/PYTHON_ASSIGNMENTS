e72393c83688bb735a24bfaeb5f248a0d16de059
Nitya Shah <ncs110905@gmail.com>
2025-08-08 18:46:07 +0530

    print a list of first # and last 5 elements

---
+++ ASSIGNMENT-1/First_Last_five_elements.py
@@ -0,0 +1,18 @@
+# Write a Python program to generate and print a list of first 
+# and last 5 elements where the values are square of 
+# numbers between 1 and 30. 
+
+l = [ 1 , 9 , 4 , 25 , 88 , 33 , 49 , 900 , 625 , 89 , 121 , 576 , 99 , 0 ]
+
+# raise the variable i to the power of 2
+all_squares = [i**2 for i in range(1, 31)]
+
+# 2. Extract the first 5 elements using list slicing.
+first_5_elements = all_squares[:5]
+
+# 3. Extract the last 5 elements using list slicing.
+last_5_elements = all_squares[-5:]
+
+# 4. Print the results.
+print("First 5 elements:", first_5_elements)
+print("Last 5 elements:", last_5_elements)
