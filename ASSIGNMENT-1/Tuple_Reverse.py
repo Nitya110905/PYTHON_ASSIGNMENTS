c5e3e0fe4b2306a9234e1f9e9a8bb67ab00f2553
Nitya Shah <ncs110905@gmail.com>
2025-08-10 00:54:00 +0530

    Reversing a Tuple

---
+++ ASSIGNMENT-1/Tuple_Reverse.py
@@ -0,0 +1,9 @@
+# Write a Python program to reverse a tuple. 
+
+
+t = (1 , 2 , 3 , "abc" , "zzz" , True)
+
+reverse_tuple = t[::-1]
+
+print(f"Original Tuple : {t}")
+print(f"Reversed : {reverse_tuple}")
