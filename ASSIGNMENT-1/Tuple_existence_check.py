ffb06361f5bee20e5ee5d9043bed9b3dbdeb7e60
Nitya Shah <ncs110905@gmail.com>
2025-08-10 00:33:19 +0530

    Existence of an element in tuple

---
+++ ASSIGNMENT-1/Tuple_existence_check.py
@@ -0,0 +1,11 @@
+# Write a Python program to check whether an element 
+# exists within a tuple. 
+
+t = (1 , 2 , 3 , "abc" , "zzz")
+
+exist = 1
+
+if exist in t:
+    print(f"Yes {exist} exists in tuple {t}")
+else:
+    print(f"No {exist}  doesn't exist in tuple {t}")
