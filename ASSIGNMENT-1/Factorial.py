265639d0805c9df5ea10131e24195794a0a0b679
Nitya Shah <ncs110905@gmail.com>
2025-08-14 17:07:27 +0530

    Factorial of a Non-negative integer

--- ASSIGNMENT-1/Factorial.py
+++ ASSIGNMENT-1/Factorial.py
@@ -3,7 +3,9 @@
 n  = int(input("Enter the no. for its factorial :"))
 
 def fact(n):
-    if n == 0 or n ==1 :
+    if (n<0):
+        return "Enter a positive integer"
+    elif n == 0 or n ==1 :
         return n
     else :
         return n*fact(n-1)
