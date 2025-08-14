49ed32ac3beeaa8923e9f2726435991899115acc
Nitya Shah <ncs110905@gmail.com>
2025-08-14 16:58:28 +0530

    List of Dictionaries

---
+++ ASSIGNMENT-1/List_of_Dictionaries.py
@@ -0,0 +1,19 @@
+# Write a Python program to combine values in python list of 
+# dictionaries. Sample data: [{'item': 'item1', 'amount': 400}, 
+# {'item': 'item2', 'amount': 300}, o {'item': 'item1', 'amount': 
+# 750}] 
+# o Expected Output: Counter ({'item1': 1150, 'item2': 
+# 300})
+
+
+from collections import Counter
+
+# Sample data
+given_list = [{'item': 'item1', 'amount': 400}, {'item': 'item2', 'amount': 300}, {'item': 'item1', 'amount': 750}]
+
+result = Counter()
+
+for d in given_list:
+    result[d['item']] += d['amount']
+
+print(result)
